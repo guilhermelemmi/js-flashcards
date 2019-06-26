591fb4ca15a752bc2926a4a5a65a15a141630114
Guilherme Lemmi <guilhermelemmi@gmail.com>
2019-06-25 23:06:41 -0300

    displaying card categories as tags

--- src/components/FlashCard.js
+++ src/components/FlashCard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Card } from 'antd';
 import { Markdown } from 'react-showdown';
+import { Tag } from 'antd';
 
 export const CARD_STATUS = {
   QUESTION: 'QUESTION',
@@ -12,6 +13,7 @@ const FlashCard = (props) => {
   return (
     <div className="Card" onClick={onClick}>
       <Card>
+        {card.tags.map((tag) => <Tag>{tag}</Tag>)}
         {cardStatus === CARD_STATUS.QUESTION
           ? (<div>Q:<Markdown markup={card.question} /></div>)
           : (<div>A:<Markdown markup={card.answer} /></div>)}
